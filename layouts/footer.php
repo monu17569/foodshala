3a2797ae1269f1c0650cd5569e04b08e92928fc2
monu17569 <monu.17569@sscbs.du.ac.in>
2022-09-16 17:48:45 +0530

    first commit

---
+++ layouts/footer.php
@@ -0,0 +1,16 @@
+<!-- footer -->
+<footer>
+        <div class="container-fluid col-md-12 row footer-div">
+            <div class="container col-md-4 row mx-auto">
+                <p class="">Dev.by Shanu Monu</p>
+                <div class="col-md social-media"> 
+                    <a href="https://www.github.com/monu17569" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="mailto: monu.17569@sscbs.du.ac.in" target="_blank"><i class="fas fa-envelope"></i></a>
+                </div>
+                
+            </div>
+        </div>
+    </footer>
+<!-- footer ends -->
+</body>
+</html>
